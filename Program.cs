9ade76ea5f42d345bccb235deea16485d7bebbbc
unknown <yourGitHub@example.com>
2020-04-05 04:18:35 +0300

    Added class(Title,Author,Content)

--- Program.cs
+++ Program.cs
@@ -4,9 +4,67 @@ namespace ConsoleAplication
 {
     class Book
     {
-        static void Main(string[] args)
+         //exrcise 3
+        public Book(string t,string a, string c)
         {
-            
+            title=new Title(t);
+            author=new Author(a);
+            content=new Content(c);
         }
+        Title title;
+        Author author;
+        Content content;
+        public void Show()
+        {
+            Console.ForegroundColor=ConsoleColor.Red;
+            title.Show();
+            Console.ForegroundColor=ConsoleColor.Green;
+            author.Show();
+            Console.ForegroundColor=ConsoleColor.Blue;
+            content.Show();
+        }
+    }
+        class Title
+        {
+            string title;
+            public Title(string t)
+            {
+                title=t;
+            }
+            public void Show()
+            {
+                System.Console.WriteLine(title);
+            }
+        }
+        class Author
+        {
+            string author;
+            public Author(string a)
+            {
+                author=a;
+            }
+            public void Show()
+            {
+                System.Console.WriteLine(author);
+            }
+        }
+        class Content
+        {
+            string content;
+            public Content(string c)
+            {
+                content=c;
+            }
+            public void Show()
+            {
+                System.Console.WriteLine(content);
+            }
+         class Program
+        {
+            static void Main(string[] args)
+            {
+            
+            }
+         }
     }
 }
